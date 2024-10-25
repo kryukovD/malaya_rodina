52bba6141d21fa1cf1dfd1e9c64838cf857e2421
Mikhail <monkeysbrain@yandex.ru>
2024-10-25 08:12:18 +0500

    page program

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function () {    
     /* mobile menu */
     $(".mobile-menu__burger").click(function () {
         $(".mobile-menu__dropdown").addClass("mobile-menu__dropdown--active");
@@ -13,6 +13,18 @@ $(document).ready(function () {
         $(this).next(".mobile-menu__children").toggleClass("mobile-menu__children--active");
     });
 
+    // Выбор элемента на странице программа
+    $('.section-program__item').click(function () {
+        $(this).toggleClass('section-program__item--selected')
+    })
+
+    // Плавный скролл к якорю
+    $('.ancor').click(function() {
+        let anchor = $(this).attr('href')
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top
+        }, 500)
+    });
 
 
     function isElementInViewport(el) {
@@ -509,7 +521,86 @@ $(document).ready(function () {
          speed:150
     });
 
- 
-     
-
 });
+
+// Драг без полосы прокрутки. При необходимости динамического добавления класса вызвать dragscroll.reset()
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else if (typeof exports !== 'undefined') {
+        factory(exports);
+    } else {
+        factory((root.dragscroll = {}));
+    }
+}(this, function (exports) {
+    var _window = window;
+    var _document = document;
+    var mousemove = 'mousemove';
+    var mouseup = 'mouseup';
+    var mousedown = 'mousedown';
+    var EventListener = 'EventListener';
+    var addEventListener = 'add'+EventListener;
+    var removeEventListener = 'remove'+EventListener;
+    var newScrollX, newScrollY;
+
+    var dragged = [];
+    var reset = function(i, el) {
+        for (i = 0; i < dragged.length;) {
+            el = dragged[i++];
+            el = el.container || el;
+            el[removeEventListener](mousedown, el.md, 0);
+            _window[removeEventListener](mouseup, el.mu, 0);
+            _window[removeEventListener](mousemove, el.mm, 0);
+        }
+
+        dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
+        for (i = 0; i < dragged.length;) {
+            (function(el, lastClientX, lastClientY, pushed, scroller, cont){
+                (cont = el.container || el)[addEventListener](
+                    mousedown,
+                    cont.md = function(e) {
+                        if (!el.hasAttribute('nochilddrag') ||
+                            _document.elementFromPoint(
+                                e.pageX, e.pageY
+                            ) == cont
+                        ) {
+                            pushed = 1;
+                            lastClientX = e.clientX;
+                            lastClientY = e.clientY;
+
+                            e.preventDefault();
+                        }
+                    }, 0
+                );
+
+                _window[addEventListener](
+                    mouseup, cont.mu = function() {pushed = 0;}, 0
+                );
+
+                _window[addEventListener](
+                    mousemove,
+                    cont.mm = function(e) {
+                        if (pushed) {
+                            (scroller = el.scroller||el).scrollLeft -=
+                                newScrollX = (- lastClientX + (lastClientX=e.clientX));
+                            scroller.scrollTop -=
+                                newScrollY = (- lastClientY + (lastClientY=e.clientY));
+                            if (el == _document.body) {
+                                (scroller = _document.documentElement).scrollLeft -= newScrollX;
+                                scroller.scrollTop -= newScrollY;
+                            }
+                        }
+                    }, 0
+                );
+             })(dragged[i++]);
+        }
+    }
+
+      
+    if (_document.readyState == 'complete') {
+        reset();
+    } else {
+        _window[addEventListener]('load', reset, 0);
+    }
+    exports.reset = reset;
+}));
